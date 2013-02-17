4db99c3b79d0a0f235acc7ffebd4a1ab112221bc
Charles LESCOT <lescot.charles@gmail.com>
2013-02-17 01:27:46 +0100

    add param1 and param2 to method 2

--- src/main/java/com/example/FooBar.java
+++ src/main/java/com/example/FooBar.java
@@ -2,7 +2,9 @@ package com.example;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
+import java.lang.reflect.ParameterizedType;
 
 @Path("/test1")
 public class FooBar {
@@ -17,7 +19,7 @@ public class FooBar {
 
     @GET
     @Path("/method2")
-    public Response test2(){
+    public Response test2(@QueryParam("param1") String param1,@QueryParam("param2") String param2){
 
         return Response.ok("good response").build();
 
