6315b286f79d9a9820c8e96d98b0c9ab0e981bc9
Charles LESCOT <lescot.charles@gmail.com>
2013-02-25 23:40:52 +0100

    remove random

--- src/main/java/com/example/FooBar.java
+++ src/main/java/com/example/FooBar.java
@@ -1,29 +1,23 @@
 package com.example;
 
 import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
-import java.lang.reflect.ParameterizedType;
 import java.util.Collection;
-import java.util.Map;
-import java.util.Random;
 
 @Path("/test1")
 public class FooBar {
 
     private static Collection coll = Lists.newArrayList();
-    private static Random random = new Random();
 
 
     @GET
-    @Path("/method1")
+    @Path("/path1")
     public Response test(@QueryParam("value") String value) throws InterruptedException {
         coll.add(value);
-        Thread.sleep(random.nextInt(1000));
         int size = coll.size();
         return Response.ok(""+size).build();
 
--- src/test/java/com/example/MultiThreadTest.java
+++ src/test/java/com/example/MultiThreadTest.java
@@ -1,11 +1,5 @@
 package com.example;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
-
-import java.util.ArrayList;
-import java.util.List;
-
 import com.google.code.tempusfugit.concurrency.ConcurrentRule;
 import com.google.code.tempusfugit.concurrency.RepeatingRule;
 import com.google.code.tempusfugit.concurrency.annotations.Concurrent;
@@ -15,57 +9,75 @@ import com.sun.jersey.api.client.WebResource;
 import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
 
 public class MultiThreadTest extends WebRunner {
+
+    private static Logger LOGGER = LoggerFactory.getLogger(MultiThreadTest.class);
 
     @Rule
     public ConcurrentRule concurrentRule = new ConcurrentRule();
 
     @Rule
     public RepeatingRule repeatingRule = new RepeatingRule();
-    private WebResource webResource1 =resource().path("/test1/method1").queryParam("value", "4");
+    private WebResource webResource1 = resource().path("/test1/path1").queryParam("value", "4");
 
     @Test
-    @Repeating(repetition = 2)
     public void testResources_without_concurrent_annotation() {
 
-
-
-        for (long size=1;size<10;size++) {
+        for (long size = 1; size < 10; size++) {
             assertThat(Long.parseLong(call(webResource1)), is(size));
         }
         WebResource webResource2 = resource().path("/test1/clear");
         call(webResource2);
+
+    }
+
+    @Test
+//    @Repeating(repetition = 2)
+    @Concurrent(count = 2)
+    public void testResources_with_concurrent_annotation() {
+
+        for (long size = 1; size < 10; size++) {
+            assertThat(Long.parseLong(call(webResource1)), is(size));
+
+            WebResource webResource2 = resource().path("/test1/clear");
+            call(webResource2);
+        }
     }
 
     @Test
     @Repeating(repetition = 2)
     @Concurrent(count = 2)
-    public void testResources_with_concurrent_annotation() {
+    public void testResources_with_concurrent_and_repeating_annotation() {
 
-
-
-        for (long size=1;size<10;size++) {
+        for (long size = 1; size < 10; size++) {
             assertThat(Long.parseLong(call(webResource1)), is(size));
+
+            WebResource webResource2 = resource().path("/test1/clear");
+            call(webResource2);
         }
-        WebResource webResource2 = resource().path("/test1/clear");
-        call(webResource2);
     }
 
     private String call(final WebResource webResource) {
+
         String response = null;
         try {
             response = webResource.get(String.class);
             assertThat("path =" + webResource.getURI() + "response=" + response, !response.isEmpty(), is(true));
-            System.out.println("response="+response);
+            LOGGER.info("response=" + response);
         } catch (UniformInterfaceException t) {
             Assert.fail(t.getMessage());
-            System.out.println("erreur message=" + t.getMessage());
-            System.out.println("erreur réponse=" + t.getResponse());
+            LOGGER.error("erreur message=" + t.getMessage());
+            LOGGER.error("erreur réponse=" + t.getResponse());
         }
 
         return response;
+
     }
 }
